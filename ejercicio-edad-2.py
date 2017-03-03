169e2c38b2769718b761c2c5b6f937ac3d291c09
ApexMaister <apexmaister@gmail.com>
2017-03-03 06:41:30 +0100

    ejercicio-edad-2.py

---
+++ ejercicio-edad-2.py
@@ -0,0 +1,7 @@
+# coding: utf­8
+edad = input("Introduce la edad:")
+
+
+if ( edad >= 18 and edad <= 23 and edad == 17 ) :
+    print "Hola"
+    print "Eres joven (aún): Puedes pasar a la sesión de jóvenes!!!!"
