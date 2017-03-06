aec506f2bbb7109f1618cc962f2163547fbefe73
ApexMaister <apexmaister@gmail.com>
2017-03-06 23:50:20 +0100

    .

---
+++ ejercicio-edad-3/ejercicio-edad-3.py
@@ -0,0 +1,16 @@
+#coding: utf-8
+import os
+
+os.system('clear')
+
+edad = input("Indica la edad:    ") 
+
+os.system('clear')
+
+if (edad >= 18 and edad <= 23) or (edad == 17):
+ 
+	
+	print " Puede entrar en la sesion de jovenes "
+
+else: 
+	print " No puedes entrar"
---
+++ ejercicio-pares/ejercicio-pares.py
@@ -0,0 +1,8 @@
+#coding: utf-8
+import os
+
+os.system('clear')
+
+numero = raw_input("Pon un numero")
+
+if numero = 
