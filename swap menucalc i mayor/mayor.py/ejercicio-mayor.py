f92bb9cedac47df813df5df4c5bbe08b14c6d6c2
ApexMaister <apexmaister@gmail.com>
2017-03-17 06:50:45 +0100

    .

---
+++ swap menucalc i mayor/mayor.py/ejercicio-mayor.py
@@ -0,0 +1,15 @@
+#coding: utf-8
+import os
+
+os.system('clear')
+print " Introdueix Un Valor"
+print " "
+valor1 = input("Numero:     ")
+
+os.system('clear')
+
+
+
+valor2 = input("Posa un Altre Numero")
+
+if 
---
+++ swap menucalc i mayor/swap.py/swap.py
@@ -0,0 +1,8 @@
+#coding: utf-8
+
+mano1 = "bocadillo"
+mano2 = "movil"
+
+manoextra = mano1
+mano1 = mano2
+mano2 = manoextra
