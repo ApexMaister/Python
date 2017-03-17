efb91f0cd9d03350d5dbfbe7655341a49e6c45a3
ApexMaister <apexmaister@gmail.com>
2017-03-17 08:50:02 +0100

    Update swap.py

--- swap menucalc i mayor/swap.py/swap.py
+++ swap menucalc i mayor/swap.py/swap.py
@@ -3,6 +3,11 @@
 mano1 = "bocadillo"
 mano2 = "movil"
 
+print  Al principi esta aixi mano1 , mano2
+
 manoextra = mano1
 mano1 = mano2
 mano2 = manoextra
+
+print Despres del swap esta aixi mano1, mano2
+
